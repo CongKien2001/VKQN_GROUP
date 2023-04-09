4ee65ac4e9fe4884814b5a65eb19474f89863199
KIEND3 <phamcongkienqn@gmail.com>
2023-04-09 20:16:49 +0700

    Revert "UT"
    This reverts commit 5cb03f0034569ff894d4beeac5b294382f6b1972.

--- vkqn_group/vkqn_group/resources/views/allcode/linkfooter.blade.php
+++
@@ -1,5 +0,0 @@
-
-    <script src="/frontend/js/media.js"></script>
-    <script src="/frontend/js/validator.js"></script>
-    <script src="/frontend/js/slider.js"></script>
-    <script src="/frontend/js/login.js"></script>
